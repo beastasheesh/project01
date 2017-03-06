f96044843aaf7ed454b01d99cdd293e980a11740
asheesh.chaudhary <asheesh.chaudhary@accenture.com>
2017-03-06 17:53:29 +0530

    Efforts 6 march

--- app/app.js
+++ app/app.js
@@ -1,4 +1,27 @@
-var myApp = angular.module('myBrewery', []);
+var myApp = angular.module('myBrewery', ['ngRoute']);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+
+  //z$locationProvider.html5Mode(true);
+
+  $routeProvider
+  .when('/home', {
+    templateUrl: 'views/home.html',
+  })
+  .when('/ourBeers', {
+    templateUrl: 'views/ourBeers.html'
+  })
+  .when('/contact', {
+    templateUrl: 'views/contact.html'
+  })
+  .when('/about', {
+    templateUrl: 'views/about.html'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  });
+
+}]);
 
 myApp.controller('MainController', ['$scope', function($scope){
 
